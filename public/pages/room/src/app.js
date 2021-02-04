b434c3013f4f4a54972c98fa4708f7d57eb33faf
Guilherme Minozzi <minozzi60@gmail.com>
2021-02-03 21:25:57 -0300

    Implementação de mais usuários na video chamada

--- public/pages/room/src/app.js
+++ public/pages/room/src/app.js
@@ -15,6 +15,16 @@ const onload = () => {
   // recorderBtn.addEventListener('click', recordClick(recorderBtn))
   const socketUrl = "http://localhost:3000";
   const socketBuilder = new SocketBuilder({ socketUrl });
+  const peerConfig = Object.values({
+    id: undefined,
+    config: {
+      port: 9000,
+      host: "localhost",
+      path: "/",
+    },
+  });
+
+  const peerBuilder = new PeerBuilder({ peerConfig });
   const view = new View();
   const media = new Media();
   const deps = {
@@ -22,6 +32,7 @@ const onload = () => {
     media,
     room,
     socketBuilder,
+    peerBuilder,
   };
   Business.initialize(deps);
 };
